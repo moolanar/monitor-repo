e4de7e50e2cbd9c9b0f606d107ccc51871d65c74
moolanar <moolanar@gmail.com>
2025-02-27 07:55:50 +0800

    Update filebeat.sh

--- filebeat.sh
+++ filebeat.sh
@@ -2,3 +2,6 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install filebeat
+
+#sudo systemctl start filebeat
+#sudo systemctl enable filebeat
